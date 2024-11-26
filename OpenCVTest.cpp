7ab42fbcb63be95a579f1a46dcfdccc1a38efeeb
Ralph Ursprung <ralph.ursprung@gmail.com>
2024-11-26 08:35:06 +0100

    aravis: WIP

--- OpenCVTest.cpp
+++ OpenCVTest.cpp
@@ -4,19 +4,39 @@
 int main() {
     bool flip = false;
 
-	cv::VideoCapture cap(0);
-
-	if(!cap.isOpened()) {
-		std::cerr << "can't use webcam" << std::endl;
-		return 1;
-	}
-
-	while (true) {
-		cv::Mat frame;
+    cv::VideoCapture cap{0, cv::CAP_ARAVIS, {
+        cv::CAP_PROP_ARAVIS_AUTOTRIGGER, 1,
+        cv::CAP_PROP_AUTO_EXPOSURE, 1,
+        //cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('G','R','B','G')
+        cv::CAP_PROP_FOURCC, cv::VideoWriter::fourcc('G','R','E','Y')
+    }};
+/*
+    if (!cap.isOpened()) {
+        std::cerr << "can't use webcam" << std::endl;
+        return 1;
+    }
+*/
+    size_t retry = 0;
+    while (true) {
+        cv::Mat frame;
         cap >> frame;
+
+        if (frame.empty()) {
+            ++retry;
+            if (retry > 10) {
+                std::cerr << "failed to get a frame 10 consecutive times!" << std::endl;
+                return 1;
+            }
+            continue;
+        } else {
+            retry = 0;
+        }
+
+        //cv::cvtColor(frame, frame, cv::COLOR_BayerGRBG2BGR);
+
         if (flip)
             cv::flip(frame, frame, 0);
-		cv::imshow("Frame | press q to quit", frame);
+        cv::imshow("Frame | press q to quit", frame);
 
         auto key = cv::waitKey(1);
         switch (key) {
@@ -28,5 +48,5 @@ int main() {
             default:
                 break;
         }
-	}
+    }
 }
